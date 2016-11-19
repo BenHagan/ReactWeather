43d3e50e2688ae869c377cfe9a0dca0acf2fd376
Ben Hagan <benhagan@users.noreply.github.com>
2016-11-18 23:14:14 -0600

    Style About component

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,14 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
+      <h1 className="text-center">About</h1>
+      <p>This app was created with the following technologies:</p>
+      <ul>
+        <li><a href="https://facebook.github.io/react/">React</a></li>
+        <li><a href="http://foundation.zurb.com/">Foundation</a></li>
+        <li><a href="https://darksky.net/forecast/">Dark Sky</a></li>
+      </ul>
+      <a href="https://github.com/BenHagan/ReactWeather">GitHub</a>
     </div>
 
   );
