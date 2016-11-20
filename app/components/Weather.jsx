13bd29df19e3e2873bdc7985dd7411b0dd039c0d
Ben Hagan <benhagan@users.noreply.github.com>
2016-11-20 08:33:17 -0600

    Add nav search support

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -13,7 +13,9 @@ var Weather = React.createClass({
   handleSearch: function (location) {
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
     });
 
     darkSky.getTemp(location).then((temp) => {
@@ -28,6 +30,22 @@ var Weather = React.createClass({
         errorMessage: e.message
       });
     });
+  },
+  componentDidMount: function () {
+    var location = this.props.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function (newProps) {
+    var location = newProps.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
   },
   render: function () {
     var {isLoading, location, temp, errorMessage} = this.state;
